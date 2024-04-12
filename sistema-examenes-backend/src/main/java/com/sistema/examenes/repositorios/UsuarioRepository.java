3967b52babbe07746da670610388a64a62f046f4
Marco Gutierrez <marcogutierrezferro@gmail.com>
2024-04-12 12:15:42 -0500

    Agregando proyecto frontend y backend

---
+++ sistema-examenes-backend/src/main/java/com/sistema/examenes/repositorios/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.sistema.examenes.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sistema.examenes.entidades.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public Usuario findByUsername(String username);
+	
+}
